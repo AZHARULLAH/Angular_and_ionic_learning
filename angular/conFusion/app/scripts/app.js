32f92752f00c920a8e3a8bbd0b603f8c4644aa3b
AZHARULLAH SHARIFF <azhar@AZHARULLAHs-MacBook-Air.local>
2017-01-07 11:33:37 +0530

    Angular ng-routing added

--- angular/conFusion/app/scripts/app.js
+++ angular/conFusion/app/scripts/app.js
@@ -1,3 +1,31 @@
-'use strict';
 
-angular.module('confusionApp', []);
+'use strict';
+angular.module('confusionApp', ['ngRoute'])
+.config(function($routeProvider, $locationProvider) {
+  $routeProvider
+  // route for the contactus page
+  .when('/contactus', {
+    templateUrl : 'contactus.html',
+    controller  : 'ContactController'
+  })
+  // route for the menu page
+  .when('/menu', {
+    templateUrl : 'menu.html',
+    controller  : 'menuController'
+  })
+  // route for the dish details page
+  .when('/menu/:id', {
+    templateUrl : 'dishdetail.html',
+    controller  : 'dishDetailController'
+  })
+  .otherwise('/contactus');
+
+  // use the HTML5 History API
+  // $locationProvider.html5Mode(true);
+
+});
+
+ $locationProvider.html5Mode({
+      enabled: true,
+      requireBase: false
+    });
--- angular/conFusion/app/scripts/services.js
+++ angular/conFusion/app/scripts/services.js
@@ -12,7 +12,7 @@ angular.module('confusionApp')
                   name:'Uthapizza',
                   image: 'images/uthapizza.png',
                   category: 'mains',
-                   label:'Hot',
+                  label:'Hot',
                   price:'4.99',
                   description:'A unique combination of Indian Uthappam (pancake) and Italian pizza, topped with Cerignola olives, ripe vine cherry tomatoes, Vidalia onion, Guntur chillies and Buffalo Paneer.',
                    comments: [
@@ -49,9 +49,9 @@ angular.module('confusionApp')
                 },
                 {
                   name:'Zucchipakoda',
-                   image: 'images/zucchipakoda.png',
+                  image: 'images/zucchipakoda.png',
                   category: 'appetizer',
-                   label:'',
+                  label:'',
                   price:'1.99',
                   description:'Deep fried Zucchini coated with mildly spiced Chickpea flour batter accompanied with a sweet-tangy tamarind sauce',
                   comments: [
@@ -88,9 +88,9 @@ angular.module('confusionApp')
                 },
                 {
                   name:'Vadonut',
-                   image: 'images/vadonut.png',
+                  image: 'images/vadonut.png',
                   category: 'appetizer',
-                   label:'New',
+                  label:'New',
                   price:'1.99',
                   description:'A quintessential ConFusion experience, is it a vada or is it a donut?',
                    comments: [
