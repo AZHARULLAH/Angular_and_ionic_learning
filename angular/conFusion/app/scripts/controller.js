fda271fa2fbaaf5fe87b2f366e45e6c42d0770dc
AZHARULLAH SHARIFF <azhar@AZHARULLAHs-MacBook-Air.local>
2017-01-07 20:41:34 +0530

    Assignment 3 complete

--- angular/conFusion/app/scripts/controller.js
+++ angular/conFusion/app/scripts/controller.js
@@ -1,79 +1,110 @@
 'use strict';
 
-angular.module("confusionApp")
+angular.module('confusionApp')
 
-.controller('menuController', ['$scope', 'menuFactory', function($scope, menuFactory)
-{
-	     $scope.tab = 1;
+        .controller('menuController', ['$scope', 'menuFactory', function($scope, menuFactory) {
+            
+            $scope.tab = 1;
+            $scope.filtText = '';
+            $scope.showDetails = false;
 
-	     $scope.filtText = "";
+            $scope.dishes= menuFactory.getDishes();
 
-       $scope.dishes = menuFactory.getDishes();
+                        
+            $scope.select = function(setTab) {
+                $scope.tab = setTab;
+                
+                if (setTab === 2) {
+                    $scope.filtText = "appetizer";
+                }
+                else if (setTab === 3) {
+                    $scope.filtText = "mains";
+                }
+                else if (setTab === 4) {
+                    $scope.filtText = "dessert";
+                }
+                else {
+                    $scope.filtText = "";
+                }
+            };
 
-        $scope.select = function(setTab)
-        {
-        	$scope.tab = setTab;
-
-        	if(setTab === 2)
-        	{
-        		$scope.filtText = "appetizer";
-        	}
-        	else if(setTab === 3)
-        	{
-        		$scope.filtText = "mains";
-        	}
-        	else if(setTab === 4)
-        	{
-        		$scope.filtText = "dessert";
-        	}
-        	else
-    		{
-        		$scope.filtText = "";
-        	}
-        }
-
-      $scope.isselected = function(checkTab)
-      {
-      	return ($scope.tab === checkTab);
-      }
-
-}])
-
-.controller('dishDetailController', [ '$scope', 'menuFactory', '$stateParams', function($scope, menuFactory, $stateParams) 
-{
-        var dish=menuFactory.getDish(parseInt($stateParams.id, 10));
-        
-        $scope.dish = dish;
+            $scope.isSelected = function (checkTab) {
+                return ($scope.tab === checkTab);
+            };
     
-}])
+            $scope.toggleDetails = function() {
+                $scope.showDetails = !$scope.showDetails;
+            };
+        }])
 
-.controller('ContactController', ['$scope', function($scope)
-{
-    $scope.feedback = {mychannel:"", firstName:"", lastName:"", agree:false, email:"" };
+        .controller('ContactController', ['$scope', function($scope) {
 
-    $scope.channels = [ {value: 'tel', label: 'Tel.'}, {value: 'Email', label: 'Email'} ];
-    $scope.invalidChannelSelection = false;
-
-}])
-
-.controller('FeedbackController', ['$scope', function($scope)
-{
-    $scope.sendFeedback = function()
-    {
-        console.log($scope.feedback);
-
-        if ($scope.feedback.agree && ($scope.feedback.mychannel == "") && !$scope.feedback.mychannel)
-        {
-            $scope.invalidChannelSelection = true;
-            console.log('incorrect');
-        }
-        else
-        {
-            $scope.invalidChannelSelection = false;
             $scope.feedback = {mychannel:"", firstName:"", lastName:"", agree:false, email:"" };
-            $scope.feedback.mychannel="";
-            $scope.feedbackForm.$setPristine();
-            console.log($scope.feedback);
-        }
-    }
-}]);
+            
+            var channels = [{value:"tel", label:"Tel."}, {value:"Email",label:"Email"}];
+            
+            $scope.channels = channels;
+            $scope.invalidChannelSelection = false;
+                        
+        }])
+
+        .controller('FeedbackController', ['$scope', function($scope) {
+            
+            $scope.sendFeedback = function() {
+                
+                console.log($scope.feedback);
+                
+                if ($scope.feedback.agree && ($scope.feedback.mychannel == "")) {
+                    $scope.invalidChannelSelection = true;
+                    console.log('incorrect');
+                }
+                else {
+                    $scope.invalidChannelSelection = false;
+                    $scope.feedback = {mychannel:"", firstName:"", lastName:"", agree:false, email:"" };
+                    $scope.feedback.mychannel="";
+                    $scope.feedbackForm.$setPristine();
+                    console.log($scope.feedback);
+                }
+            };
+        }])
+
+        .controller('dishDetailController', ['$scope', '$stateParams', 'menuFactory', function($scope, $stateParams, menuFactory) {
+
+            var dish= menuFactory.getDish(parseInt($stateParams.id,10));
+            
+            $scope.dish = dish;
+            
+        }])
+
+        .controller('DishCommentController', ['$scope', function($scope) {
+            
+            $scope.mycomment = {rating:5, comment:"", author:"", date:""};
+            
+            $scope.submitComment = function () {
+                
+                $scope.mycomment.date = new Date().toISOString();
+                console.log($scope.mycomment);
+                
+                $scope.dish.comments.push($scope.mycomment);
+                
+                $scope.commentForm.$setPristine();
+                
+                $scope.mycomment = {rating:5, comment:"", author:"", date:""};
+            }
+        }])
+
+        // implement the IndexController and About Controller here
+
+        .controller('AboutController', ['$scope', 'corporateFactory', function($scope, corporateFactory)
+        {
+            $scope.corporateLeadership = corporateFactory.getLeaders();
+        }])
+
+        .controller('IndexController', ['$scope', 'corporateFactory', 'menuFactory', function($scope, corporateFactory, menuFactory)
+        {
+            $scope.recommendedDish = menuFactory.getDish(1);
+            $scope.monthPromotions = menuFactory.getPromotion()
+            $scope.executiveChef = corporateFactory.getLeader(2);
+        }])
+
+;
